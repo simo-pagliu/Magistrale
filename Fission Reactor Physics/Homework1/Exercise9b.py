7b72f623b5626ec56d8513ed1d6691df1aeaeec5
Simone Pagliuca <pagliuca.simone01@gmail.com>
2024-08-30 16:57:44 +0200

    better plot for ex 9 hw1

--- Fission Reactor Physics/Homework1/Exercise9b.py
+++ Fission Reactor Physics/Homework1/Exercise9b.py
@@ -23,14 +23,34 @@ Sm_Sol = np.array(data["Sm-149 [atoms]"])
 # Plot the concentrations
 ############################################################################################################
 plt.figure()
-plt.plot(t_vals, Nd_Sol, label='Nd-140')
-plt.plot(t_vals, Pm_Sol, label='Pm-149')
 plt.plot(t_vals, Sm_Sol, label='Sm-149')
 plt.xlabel("Time [s]")
 plt.ylabel("Concentration [#atoms]")
-plt.title("Concentration of isotopes over time")
+plt.title("Concentration of Sm-149 over time")
 plt.legend()
 plt.grid()
 # save the plot to a file
-plt.savefig(".\Fission Reactor Physics\Homework1\Sol_9.png")
+plt.savefig(".\Fission Reactor Physics\Homework1\Sol_9a.png")
+plt.show()
+
+plt.figure()
+plt.plot(t_vals, Pm_Sol, label='Pm-149')
+plt.xlabel("Time [s]")
+plt.ylabel("Concentration [#atoms]")
+plt.title("Concentration of Pm-149 over time")
+plt.legend()
+plt.grid()
+# save the plot to a file
+plt.savefig(".\Fission Reactor Physics\Homework1\Sol_9b.png")
+plt.show()
+
+plt.figure()
+plt.plot(t_vals, Nd_Sol, label='Nd-140')
+plt.xlabel("Time [s]")
+plt.ylabel("Concentration [#atoms]")
+plt.title("Concentration of Nd-140 over time")
+plt.legend()
+plt.grid()
+# save the plot to a file
+plt.savefig(".\Fission Reactor Physics\Homework1\Sol_9c.png")
 plt.show()
